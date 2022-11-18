b78daf51de4cbfc529b9376e1ea9346e223a8fed
Destinee Fa <destinee.fa@gmail.com>
2022-11-18 17:30:52 +0800

    progress on profile updating

---
+++ src/forms/profile/Change.jsx
@@ -0,0 +1,155 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import { useNavigate } from 'react-router-dom'
+
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+import { useUpdateMyProfileMutation, useCreateMyProfileMutation } from '@/services/api/Profile'
+
+const initialValues = {
+  currentJob: '',
+  highestEducation: '',
+  about: ''
+}
+
+function FormsProfileChangeModal(props) {
+  const navigate = useNavigate()
+  const [createMyProfile] = useCreateMyProfileMutation()
+  const [updateMyProfile, { isSuccess }] = useUpdateMyProfileMutation()
+
+  const handleSubmit = props.initialValues ? (
+
+    async (values) => {
+      await updateMyProfile(values)
+        .then(() => {
+          if (isSuccess) {
+            { props.onHide }
+          }
+          navigate('/my/profile')
+        })
+    }
+
+  ) : (
+    (values) => {
+      createMyProfile(values).unwrap().then(() => {
+        console.log(values)
+        navigate('/my/profile')
+      })
+    }
+
+  )
+
+  return (
+    <Modal
+      {...props}
+      backdrop="static"
+      keyboard={false}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Enter your profile information
+        </Modal.Title>
+      </Modal.Header>
+
+      <Formik
+        initialValues={props.initialValues || initialValues}
+        onSubmit={handleSubmit}
+        enableReinitialize
+        validationSchema={
+        Yup.object({
+          currentJob: Yup.string().required().trim().label('Current job/job status'),
+          highestEducation: Yup.string().trim().required().label('Institution of highest level of education'),
+          about: Yup.string().trim().required().label('About me')
+        })
+      }
+      >
+        {
+        ({ errors: e, touched: t, isSubmitting }) => (
+          <Form>
+            <Modal.Body>
+              <div className="mb-3">
+                <label>Current employment/employment status</label>
+                <Field
+                  className={`form-control ${e?.currentJob && t?.currentJob && 'is-invalid'}`}
+                  name="currentJob"
+                  placeholder="What is your current job or job status?"
+                />
+                <ErrorMessage
+                  className="invalid-feedback"
+                  name="currentJob"
+                  component="div"
+                />
+              </div>
+
+              <div className="mb-3">
+                <label>Institution of highest level of education</label>
+                <Field
+                  className={`form-control ${e?.highestEducation && t?.highestEducation && 'is-invalid'}`}
+                  name="highestEducation"
+                  placeholder="Where did you get your highest level of education?"
+                />
+                <ErrorMessage
+                  className="invalid-feedback"
+                  name="highestEducation"
+                  component="div"
+                />
+              </div>
+
+              <div className="mb-3">
+                <label>About you</label>
+                <Field
+                  className={`form-control ${e?.about && t?.about && 'is-invalid'}`}
+                  name="about"
+                  as="textarea"
+                  rows="5"
+                  placeholder="Share a little bit about yourself! What insight can you offer? What questions do you want answers for?"
+                />
+                <ErrorMessage
+                  className="invalid-feedback"
+                  name="about"
+                  component="div"
+                />
+              </div>
+
+            </Modal.Body>
+
+            {
+              props.initialValues ? (
+                <Modal.Footer>
+                  <Button variant="outline-secondary" onClick={props.onHide}>
+                    Cancel
+                  </Button>
+
+                  <Button
+                    className="btn btn-dark"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >Save Changes</Button>
+                </Modal.Footer>
+              ) : (
+
+                <Modal.Footer>
+                  <Button
+                    className="btn btn-dark"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >Create Profile</Button>
+                </Modal.Footer>
+
+              )
+            }
+
+          </Form>
+        )
+      }
+      </Formik>
+    </Modal>
+  )
+}
+
+export default FormsProfileChangeModal
--- src/pages/my/Profile.jsx
+++ src/pages/my/Profile.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 import Skeleton from 'react-loading-skeleton'
 
 import { useGetMyProfileQuery } from '@/services/api/Profile'
 
-function MyProfile({ profile }) {
+import FormsProfileChangeModal from '@/forms/profile/Change'
+
+function MyProfile({ profile, show, onClick, onHide }) {
   return (
-    <>
+    <div className="p-5 mb-4 bg-light border rounded-3">
       <div className="d-flex flex-row justify-content-end mb-2">
-        <button className="btn btn-outline-secondary btn-sm" type="button">Edit</button>
+        <button
+          className="btn btn-outline-secondary btn-sm"
+          type="button"
+          onClick={onClick}
+        >Edit</button>
       </div>
+
+      <FormsProfileChangeModal initialValues={profile} show={show} onHide={onHide} />
+
       <div className="row g-4 px-4 row-cols-1 row-cols-lg-2">
 
         <div className="col">
@@ -24,26 +34,33 @@ function MyProfile({ profile }) {
             </div>
             <div className="col">
               <h3 className="fs-4 fw-bold mb-4">{profile.user.fullName}</h3>
-              <h5 className="mb-2">{profile.currentJob}</h5>
-              <h5>{profile.highestEducation}</h5>
+              <h6 className="mb-2">{profile.currentJob}</h6>
+              <h6>{profile.highestEducation}</h6>
             </div>
           </div>
         </div>
 
         <div className="d-flex flex-column justify-content-top px-5">
-          <h3 className="fs-4">About Me</h3>
+          <h5>About Me</h5>
           <p>{profile.about}</p>
         </div>
 
       </div>
-    </>
+    </div>
+  )
+}
+
+function MyProfileModal(props) {
+  return (
+    <FormsProfileChangeModal show={props.show} />
   )
 }
 
 function PagesMyProfile() {
-  // try getting profile, not successful then pop up creating modal
-  // on submission show profile
-  // update=modal
+  // submit=updates profile
+
+  const [createModalShow, setCreateModalShow] = React.useState(false)
+  const [editModalShow, setEditModalShow] = React.useState(false)
 
   const {
     data: profile,
@@ -53,21 +70,42 @@ function PagesMyProfile() {
     error
   } = useGetMyProfileQuery()
 
+  useEffect(() => {
+    if (!profile && !createModalShow) {
+      setCreateModalShow(true)
+    }
+    if (profile && createModalShow) {
+      setCreateModalShow(false)
+    }
+  }, [profile, createModalShow])
+
   let content
 
   if (isLoading) {
-    content = <div><Skeleton count={5} /></div>
+    content = (
+      <div className="p-5 mb-4 bg-light border rounded-3">
+        <Skeleton count={5} />
+      </div>
+    )
+  } else if (!profile) {
+    content = ''
   } else if (isSuccess) {
-    content = <MyProfile profile={profile} />
+    content = (
+      <MyProfile
+        profile={profile}
+        show={editModalShow}
+        onClick={() => setEditModalShow(true)}
+        onHide={() => setEditModalShow(false)}
+      />
+    )
   } else if (isError) {
     content = <div>{error.toString()}</div>
   }
 
   return (
     <div id="pages-my-profile" className="container p-5">
-      <div className="p-5 mb-4 bg-light border rounded-3">
-        {content}
-      </div>
+      {content}
+      <MyProfileModal show={createModalShow} />
     </div>
   )
 }
