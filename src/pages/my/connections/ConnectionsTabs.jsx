66f283654645cd9956a7fb8914dd7a516b82700e
Destinee Fa <destinee.fa@gmail.com>
2022-11-25 12:10:48 +0800

    changed connections tabs to reset search params

--- src/pages/my/connections/ConnectionsTabs.jsx
+++ src/pages/my/connections/ConnectionsTabs.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
 import FollowingTab from './FollowingTab'
 import FollowersTab from './FollowersTab'
 
 function ConnectionsTabs() {
+  const [searchParams, setSearchParams] = useSearchParams()
   const [key, setKey] = useState('following')
+
+  const removeQueryParams = () => {
+    const param = searchParams.get('page')
+    if (param) {
+      searchParams.delete('page')
+      setSearchParams(searchParams)
+    }
+  }
 
   return (
     <div className="container my-5 px-4">
       <Tabs
         id="connections-tabs"
         activeKey={key}
-        onSelect={(k) => setKey(k)}
+        onSelect={(k) => {
+          setKey(k)
+          removeQueryParams()
+        }}
         className="mb-3"
       >
         <Tab eventKey="following" title="Following">
